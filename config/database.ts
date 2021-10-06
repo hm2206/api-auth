07fd5687fc0b2e52ee4884bcac5f8fa10d7dcc94
Hans <twd2206@gmail.com>
2021-10-06 18:07:59 -0500

    fix db

--- config/database.ts
+++ config/database.ts
@@ -36,11 +36,11 @@ const databaseConfig: DatabaseConfig = {
     mysql: {
       client: 'mysql',
       connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
+        host: Env.get('MYSQL_HOST', ''),
+        port: Env.get('MYSQL_PORT', ''),
+        user: Env.get('MYSQL_USER', ''),
         password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
+        database: Env.get('MYSQL_DB_NAME', ''),
       },
       migrations: {
         naturalSort: true,
